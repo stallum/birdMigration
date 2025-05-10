16cdd253b06d26456c10ddcc17cdbd03a51c3e14
Artur Gabriel "stallum <stallumgp8@gmail.com>
2025-05-10 20:53:31 -0300

    resolvendo erros

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+import dash
+from dash import html, dcc
+from charts import createFigures
 import pandas as pd
+from utils import loadData
 
-dataset = pd.read_csv('raw_data/bird_migration_data.csv')
-print(dataset)
+# Carrega os dados
+df = loadData()
 
-sumaryStats = dataset.describe()
+# Gera os gráficos
+fig1, fig2, fig3, fig4, fig5 = createFigures(df)
+
+# Inicializa o app
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Dashboard de Migração de Aves", style={'textAlign': 'center'}),
+
+    dcc.Graph(figure=fig1),
+    dcc.Graph(figure=fig2),
+    dcc.Graph(figure=fig3),
+    dcc.Graph(figure=fig4),
+    dcc.Graph(figure=fig5),
+])
+
+if __name__ == "__main__":
+    app.run(debug=True)
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-def loadData(df): 
+def loadData(): 
     return pd.read_csv('raw_data/bird_migration_data.csv')
